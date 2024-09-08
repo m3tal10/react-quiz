584e0051909c2386a5adea729fc50e5fc1d83305
Mashrafie <rahimsheikh05@gmail.com>
2024-09-08 19:34:56 +0600

    StartScreen import typo solved.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Loader from "./Components/Loader";
 import Error from "./Components/Error";
-import StartScreen from "./Components/startScreen";
+import StartScreen from "./Components/StartScreen";
 import Question from "./Components/Question";
 import NextButton from "./Components/NextButton";
 import Progress from "./Components/Progress";
